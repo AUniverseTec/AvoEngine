c9cc74d37f45778b657b0412e50dd23293b0a366
AUniverse <avoteamoficial@Gmail.com>
2024-10-13 12:59:42 +0300

    Create animation_loader.go

---
+++ go/avoeengine/animation_loader.go
@@ -0,0 +1,11 @@
+package animation_loader
+
+import (
+	"go/avoeengine/animation_system"
+)
+
+func Load(path string) *animation_system.Animation {
+	// Load the animation from the file
+	// ...
+	return &animation_system.Animation{}
+}
