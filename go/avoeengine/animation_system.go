d578881b805c9afbcf7aa1fbaff0d671b4036195
AUniverse <avoteamoficial@Gmail.com>
2024-10-13 12:56:58 +0300

    Create animation_system.go

---
+++ go/avoeengine/animation_system.go
@@ -0,0 +1,21 @@
+package animation_system
+
+import (
+	"github.com/go-gl/mathgl/mgl32"
+)
+
+type Animation struct {
+	Duration float32
+	StartTime float32
+	MeshVertices []mgl32.Vec3
+}
+
+func Init() {
+	// Initialize the animation system
+	// ...
+}
+
+func Update() {
+	// Update the animation system
+	// ...
+}
