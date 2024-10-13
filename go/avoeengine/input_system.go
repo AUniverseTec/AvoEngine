5d0fc06ee720c59cb2ca6cc21d469ee4e6d9caa9
AUniverse <avoteamoficial@Gmail.com>
2024-10-13 12:57:19 +0300

    Create input_system.go

---
+++ go/avoeengine/input_system.go
@@ -0,0 +1,15 @@
+package input_system
+
+import (
+	"github.com/go-gl/glfw/v3.3/glfw"
+)
+
+func Init() {
+	// Initialize the input system
+	// ...
+}
+
+func Update() {
+	// Update the input system
+	// ...
+}
